a0b59c9786421d1165ea7425c5286adbe73eca2f
Andahyun <adh2824@gmail.com>
2019-11-22 15:37:45 +0900

    8

---
+++ rollover.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	
+	$('.rollover').each(function(){
+		var a = $(this);
+		var img = a.find('img');
+		var srcOff = img.attr('src');
+		var srcOn = srcOff.replace('-off','-on');
+		$('<img>').attr('src',srcOn);
+		a.on({
+			'mouseenter focus':function(){img.attr('src',srcOn);},
+			'mouseleave blur':function(){img.attr('src',srcOff);}
+		});
+		/*a.hover(function(){
+			img.attr('src',srcOn);
+		},function(){
+			img.attr('src',srcOff);
+		});*/
+	});
+	
+});
