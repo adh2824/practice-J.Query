b2366be24d8dd6d93fb99dc36d3498fac7c7cd18
Andahyun <adh2824@gmail.com>
2019-11-22 15:34:13 +0900

    3

---
+++ selector.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	
+	$('#div1').css('background','lightblue');
+	$('.women').css({
+		'background':'black',
+		'color':'white',
+		'border-style':'dashed'
+	});
+	$('.men').css('background','yellowgreen');
+	
+});
